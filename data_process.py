0a16d30ca8b9a4f90ffbfa322f69603d192c58c9
izuminka <dolin@ucsb.edu>
2019-09-04 15:31:09 +0300

    gener. load_data image type, added scaled_gray_data_load to data_process

--- data_process.py
+++ data_process.py
@@ -18,9 +18,11 @@ def load_data(data_dir):
                     os.path.isdir(f"{data_dir}/{d}")]
 
     images, labels = [], []
+    # TODO add more image extensions
+    image_ext = (".ppm", ".jpeg", ".jpg")
     for label in group_labels:
         file_paths_group = [f"{data_dir}/{label}/{f}" for f in
-                            os.listdir(f"{data_dir}/{label}") if f.endswith(".ppm")]
+                            os.listdir(f"{data_dir}/{label}") if f.endswith(image_ext)]
         for image_file in file_paths_group:
             images.append(skimage.data.imread(image_file))
             labels.append(int(label))
@@ -41,31 +43,57 @@ def resize_images(images, size):
     """
     return np.array([skimage.transform.resize(img, (size, size)) for img in images])
 
-def get_gray_train_test():
+
+def scaled_gray_data_load(file_name):
     """Quick loading and processing of the data for easier training and testing
-        of the NN
+            of the NN
+
+    Args:
+        file_name (str): Name of the data directory, ex: training, testing, new_images_set1
 
     Returns:
-        tuple: images_train, lables_train, images_test, lables_test
-               images_* (numpy.ndarray): each item is image of shape (new_im_size, new_im_size)
-               lables_* (numpy.ndarray): each item is int label
+        tuple: images, lables
+           images (numpy.ndarray):
+           each item is image of shape (new_im_size, new_im_size)
+           lables (numpy.ndarray):
+           each item is int label (name of the directory within file_name)
 
     """
     new_im_size = 28
     root_path = os.path.dirname(os.path.dirname(os.path.realpath("__file__")))
-    train_work_dir = f"{root_path}/data/training"
-    test_work_dir = f"{root_path}/data/testing"
-    train_test = []
-    for dir_type in [train_work_dir, test_work_dir]:
-        # load raw images
-        images, labels = load_data(dir_type) # same l and w
-        # resize all images to the the same l and w
-        images_28_28 = resize_images(images, new_im_size) #shape (?, 28, 28, 3)
-        # gray out all resized images, now shape (?, 28, 28)
-        images_28_28_gray = skimage.color.rgb2gray(images_28_28)
-        train_test.append([images_28_28_gray, labels])
-            # images_train, lables_train, images_test, lables_test
-    return train_test[0][0], train_test[0][1], train_test[1][0], train_test[1][1]
+    dir_type = f"{root_path}/data/{file_name}"
+    images, labels = load_data(dir_type) # same l and w
+    # resize all images to the the same l and w
+    images_28_28 = resize_images(images, new_im_size) #shape (?, 28, 28, 3)
+    # gray out all resized images, now shape (?, 28, 28)
+    images_28_28_gray = skimage.color.rgb2gray(images_28_28)
+    return images_28_28_gray, labels
+
+# def get_gray_train_test():
+#     """Quick loading and processing of the data for easier training and testing
+#         of the NN
+#
+#     Returns:
+#         tuple: images_train, lables_train, images_test, lables_test
+#                images_* (numpy.ndarray): each item is image of shape (new_im_size, new_im_size)
+#                lables_* (numpy.ndarray): each item is int label
+#
+#     """
+#     new_im_size = 28
+#     root_path = os.path.dirname(os.path.dirname(os.path.realpath("__file__")))
+#     train_work_dir = f"{root_path}/data/training"
+#     test_work_dir = f"{root_path}/data/testing"
+#     train_test = []
+#     for dir_type in [train_work_dir, test_work_dir]:
+#         # load raw images
+#         images, labels = load_data(dir_type) # same l and w
+#         # resize all images to the the same l and w
+#         images_28_28 = resize_images(images, new_im_size) #shape (?, 28, 28, 3)
+#         # gray out all resized images, now shape (?, 28, 28)
+#         images_28_28_gray = skimage.color.rgb2gray(images_28_28)
+#         train_test.append([images_28_28_gray, labels])
+#             # images_train, lables_train, images_test, lables_test
+#     return train_test[0][0], train_test[0][1], train_test[1][0], train_test[1][1]
 
 # images_train, lables_train, images_test, lables_test = get_gray_train_test()
 # print((lables_train[0]))
